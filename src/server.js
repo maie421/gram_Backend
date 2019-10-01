a7c56f762d9286036001f1f5c3065c7b4ba74479
윤영미 <35258834+maie421@users.noreply.github.com>
2019-10-01 16:44:25 +0900

    중복 x , request

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import dotenv from "dotenv";//숨겨진 파일 확인
-import path from "path";
-//console.log(`주소:${__dirname}`);
-dotenv.config({path:path.resolve(__dirname,".env")});
-
+import "./env";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
-import passport from "passport";
 import "./passport";
+import {authenticateJwt} from "./passport";
 
 //sendSecretMail("maie421@naver.com", "123");
 const PORT = process.env.PORT || 2000;
 
-const server = new GraphQLServer({schema});
+const server = new GraphQLServer({
+  schema,
+  context:({request})=>({request})
+});
 
 server.express.use(logger("dev"));
+server.express.use(authenticateJwt);
 //server.express.use(passport.authenticate("jwt"));
 
 server.start({ port: PORT }, () =>
