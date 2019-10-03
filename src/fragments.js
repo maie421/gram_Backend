d4d864a3aff7a444575f4f09c4c2381d976d83c2
윤영미 <35258834+maie421@users.noreply.github.com>
2019-10-04 00:11:00 +0900

    computed

--- src/fragments.js
+++ src/fragments.js
@@ -1,9 +1,34 @@
-export const COMMENT_FRAGMENT  = `
-    fragment CommentParts on comments{
-        id 
-        text
-        user{
-            username
+
+export const USER_FRAGMENT = `
+    id
+    username
+`;
+
+export const COMMENT_FRAGMENT = `
+    id
+    text
+    user {
+        ${USER_FRAGMENT}
+    }
+`;
+
+export const FILE_FRAGMENT = `
+    id
+    url
+`;
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post{
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
         }
     }
 `;
